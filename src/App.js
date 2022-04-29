d6f84a3d3dfb2df0b6be32a7703d4285640e15fe
dombatk <dodek4562@gmail.com>
2022-04-29 19:23:25 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizComponent from "./components/QuizComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <QuizComponent/>
+  );
+}
+
+export default App;
